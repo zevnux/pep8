49623d586f1bf3e16495efbafa059aed74b91dd5
Carol Fu <fu.carol975@gmail.com>
2015-11-23 16:58:02 -0800

    ISSUETESTONE FOR PYTHON2 POINT ONE

---
+++ ISSUETEST.py
@@ -0,0 +1,11 @@
+#: TAB ERROR TEST
+for a in 'abc':
+	for b in 'xyz':
+	     print a # indented with 5 spaces
+				# should generate a "TAB ERROR: TAB INDENTATION EXPECTED; 5 SPACES WERE USED"
+				
+    print b # indented with 4 spaces
+	
+	
+
+
